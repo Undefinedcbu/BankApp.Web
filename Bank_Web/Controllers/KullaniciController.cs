ac8f0b0826c246172b325d707c6ae54df3cb93b9
iremaslandogan <iremaslandogan@gmail.com>
2019-12-10 20:27:05 +0300

    Add project files.

---
+++ Bank_Web/Controllers/KullaniciController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bank_Web.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        // GET: Kullanici
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hesap()
+        {
+            return View();
+        }
+        public ActionResult Virman()
+        {
+            return View();
+        }
+        public ActionResult Havale()
+        {
+            return View();
+        }
+        public ActionResult Fatura()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Bank_Web/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankAppWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult KayıtOl()
+        {
+            return View();
+        }
+    }
+}
